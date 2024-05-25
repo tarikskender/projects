aac24125ab321418c6e6c2b926faf7f531e4f2df
tarikskender <tarikskender23@gmail.com>
2024-05-26 00:53:45 +0200

    Spapp add and fixes

--- js/spapp.js
+++ js/spapp.js
@@ -1,10 +1,35 @@
 $(document).ready(function () {
+  $(".close-btn").click(function () {
+    $("#popup").addClass("hidden");
+  });
+
   $(".button-19").click(function () {
     $("#popup").removeClass("hidden");
-    var app = $.spapp({
-      defaultView: "#page1",
-      templateDir: "./views/",
+
+    const parent = $("#spapp");
+
+    parent.find("section").each(function () {
+      var pageToLoad = $(this).data("load");
+
+      var app = $.spapp({
+        defaultView: "#" + this.id,
+        templateDir: "/views/",
+        pageNotFound: "404.html",
+        beforeLoad: function () {
+          console.log(`Loading content for ${this.id} from ${pageToLoad}`);
+        },
+      });
+
+      if (pageToLoad) {
+        $(this).load(`/views/${pageToLoad}`, function (response, status, xhr) {
+          if (status == "error") {
+            console.log(`Error loading the page: ${xhr.statusText}`);
+          } else {
+            console.log(`Page loaded successfully: ${pageToLoad}`);
+            app.run();
+          }
+        });
+      }
     });
-    app.run();
   });
 });
